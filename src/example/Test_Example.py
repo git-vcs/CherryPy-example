ff54b34539d4c5465999142485bdda7583e9b5c0
Mikael Nervik Eidsvaag <58994869+git-vcs@users.noreply.github.com>
2021-07-17 13:07:51 +0200

    Added a simple test 	new file:   src/example/Test_Example.py

---
+++ src/example/Test_Example.py
@@ -0,0 +1,9 @@
+import unittest
+from Example import Example;
+
+class TestExample(unittest.TestCase):
+    def test_postRealay(self):
+        respone = str(Example.postExample("unittest")[1])
+        print("printing:",respone['message'].split(":")[1])
+        self.assertEqual(respone,"unittest")
+        
